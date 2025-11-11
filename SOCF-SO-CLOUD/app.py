cbfef9cb130c5311e8310f28fd01b344fbf5c1c2
Brenda Barbosa <151410709+brebarbosaa@users.noreply.github.com>
2025-11-11 13:27:14 +0000

    initial commit

---
+++ SOCF-SO-CLOUD/app.py
@@ -0,0 +1,26 @@
+import platform
+import psutil
+import os
+import json
+
+# Obtém versão do S.O. subjacente
+print(platform.platform())
+
+# Obtém PID
+print(os.getpid())
+
+#Obtém uso de CPU em porcentagem
+print(psutil.cpu_percent())
+
+# Obtém uso de memória em MB
+print(psutil.vitual_memory().used // 1024 ** 2)
+
+metricas = {
+    'so' : platform.platform(),
+    'pid' : os.getpid(),
+    'uso_cpu' : psutil.cpu_percent(),
+    'memoria_mb': psutil.virtual_memory().used // 1024 ** 2
+}
+
+# transforma resultados em texto e ignora ascii
+print(json.dumps(metricas, ensure_ascii=False))
