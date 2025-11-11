7aa440c8448fcc6e8ab02304c9f4d46065184db2
Brenda Barbosa <151410709+brebarbosaa@users.noreply.github.com>
2025-11-11 18:12:23 +0000

    update

--- app.py
+++ app.py
@@ -4,23 +4,25 @@ import psutil
 import os
 import json
 
-APP = Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/info')
-def info():
-    return "Brenda Gabrielli Barbosa"
-
-
-@app.route('/metricas')
-def metricas():
-    metricas = {
+metricas = {
     'so' : platform.platform(),
     'pid' : os.getpid(),
     'uso_cpu' : psutil.cpu_percent(),
     'memoria_mb': psutil.virtual_memory().used // 1024 ** 2
     }   
+
+@app.route('/info')
+def info():
+    return "Brenda Gabrielli Barbosa"
+
+@app.route('/metricas')
+def metricas():
     # transforma resultados em texto e ignora ascii
     return json.dumps(metricas, ensure_ascii=False)
 
+APP = app
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
