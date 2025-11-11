ffb574435dc3a2561d374ab292393ef772666f74
Brenda Barbosa <151410709+brebarbosaa@users.noreply.github.com>
2025-11-11 18:30:41 +0000

    atualização rotas

--- app.py
+++ app.py
@@ -15,15 +15,15 @@ def home():
 
 @app.route('/info')
 def info():
-    return "Brenda Gabrielli Barbosa"
+    return "Aluna: Brenda Gabrielli Barbosa"
 
 @app.route('/metricas')
 def exibe_metricas():
     metricas = {
-        'so' : platform.platform(),
-        'pid' : os.getpid(),
-        'uso_cpu' : psutil.cpu_percent(),
-        'memoria_mb': psutil.virtual_memory().used // 1024 ** 2
+        'Sistema Operacional:' : platform.platform(),
+        'PID:' : os.getpid(),
+        'Uso de CPU:' : psutil.cpu_percent(),
+        'Memoria utilizada:': psutil.virtual_memory().used // 1024 ** 2
     }  
     # transforma resultados em texto e ignora ascii
     return json.dumps(metricas, ensure_ascii=False)
